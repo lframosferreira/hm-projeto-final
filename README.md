c20191f0caf3820e8ad5ee25f775c4a6116d96af
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-11-20 09:52:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Heurísticas e Metaheurísticas
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+  std::cout << "Oie" << std::endl;
+  exit(EXIT_SUCCESS);
+}
