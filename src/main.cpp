0c963f0bf51d46a708eb405c0c98c43bd391ddf3
Raul Araju <raularaju@uol.com.br>
2025-01-20 23:16:08 -0300

    grid search ga

--- src/main.cpp
+++ src/main.cpp
@@ -77,50 +77,34 @@ void test(const std::string &graph_file, int num_executions, const std::string &
 	Graph g;
 	g.read_edge_list(graph_file);
 
-	int best_clique = 0;
-	double total_time = 0.0;
-	for (int i = 0; i < num_executions; ++i)
+	std::vector<double> list_pcs = {0.1, 0.3, 0.5, 0.7, 0.9};
+	std::vector<double> list_pms = {0.1, 0.3, 0.5, 0.7, 0.9};
+	std::vector<int> list_pop = {50, 100, 200, 300};
+	std::vector<int> list_gens = {50, 100, 200, 300};
+	auto start = std::chrono::high_resolution_clock::now();
+	for (auto const pc : list_pcs)
 	{
-		auto start = std::chrono::high_resolution_clock::now();
-
-		GeneticAlgorithm ga(g, 100, 0.9, 0.1, 100);
-		std::cout << "Running genetic algorithm..." << std::endl;
-		std::vector<int> clique = ga.run();
-		int clique_size_ga = clique.size();
-		std::cout << "Clique size ga: " << clique_size_ga << std::endl;
-		double exec_time_ga = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
-
-		/* SimulatedAnnealing sa(100.0, 0.001, 0.9995, g, clique_size_ga, clique);
-		std::cout << "Running simulated annealing algorithm..." << std::endl;
-		clique = sa.run();
-		int clique_size_sa = clique.size();
-		std::cout << "Clique size sa: " << clique_size_sa << std::endl;
-		double exec_time_sa = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
-
-		TabuSearch ts(g, clique, 10, 100);
-		std::cout << "Running tabu search..." << std::endl;
-		ts.run();
-
-		int cur_clique = ts.getBestClique();
-		std::cout << "Clique size ts: " << cur_clique << std::endl;
-		std::cout << "Done" << std::endl;
-		if (cur_clique > best_clique)
+		for (auto const pm : list_pms)
 		{
-			best_clique = cur_clique;
+			for (auto const pop : list_pop)
+			{
+				for (auto const gens : list_gens)
+				{
+					for (int i = 0; i < num_executions; ++i)
+					{
+						GeneticAlgorithm ga(g, pop, pc, pm, gens);
+						std::cout << "Running genetic algorithm..." << std::endl;
+						std::vector<int> clique = ga.run();
+						int clique_size_ga = clique.size();
+						std::cout << "Clique size ga: " << clique_size_ga << std::endl;
+						double exec_time_ga = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
+						out << (i + 1) << "," << graph_file_pretty << "," << clique_size_ga << "," << exec_time_ga << ","
+							<< pc << "," << pm << "," << pop << "," << gens << std::endl;
+					}
+				}
+			}
 		}
-		auto end = std::chrono::high_resolution_clock::now();
-
-		total_time += std::chrono::duration<double>(end - start).count();
-		double exec_time = std::chrono::duration<double>(end - start).count();
-
-		out << (i + 1) << "," << graph_file_pretty << ","
-		<< clique_size_ga << "," << clique_size_sa << "," << cur_clique
-		<< "," << exec_time_ga << "," << exec_time_sa << "," << exec_time
-			<< std::endl; */
-		out << (i + 1) << "," << graph_file_pretty << "," << clique_size_ga << "," << exec_time_ga << std::endl;
 	}
-	std::cout << "Melhor clique em " << num_executions << " execucoes = " << best_clique << std::endl;
-	std::cout << "Tempo de execucao total: " << total_time << " seconds" << std::endl;
 }
 
 void test_all_files_in_directory(const std::string &directory_path, int num_executions, const std::string &output_file)
@@ -136,7 +120,7 @@ void test_all_files_in_directory(const std::string &directory_path, int num_exec
 			{
 				std::string file_path = directory_path + "/" + file_name;
 				std::cout << "Testing file: " << file_path << std::endl;
-				control_test(file_path, num_executions, output_file);
+				test(file_path, num_executions, output_file);
 			}
 		}
 		closedir(dir);
@@ -157,7 +141,7 @@ int main(int argc, char **argv)
 
 	std::string directory_path = argv[1];
 	int num_executions = 20;
-	std::string output_file = "output2.csv";
+	std::string output_file = "grid_search_ga.csv";
 
 	test_all_files_in_directory(directory_path, num_executions, output_file);
 
