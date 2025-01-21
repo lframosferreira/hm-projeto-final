13b69bb2fae5ea554cecfdbaacc682f5b7badd9f
Giovana2ma <giovana2ma@gmail.com>
2025-01-20 21:44:34 -0300

    ajustes

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,45 @@ std::string extract_file_name(const std::string &file_path)
 	}
 	return file_name;
 }
+void control_test(const std::string &graph_file, int num_executions, const std::string &output_file)
+{
+	std::ofstream out(output_file, std::ios::app);
+	if (!out.is_open())
+	{
+		std::cerr << "Erro ao abrir o arquivo de output: " << output_file << std::endl;
+		return;
+	}
+	std::string graph_file_pretty = extract_file_name(graph_file);
+	Graph g;
+	g.read_edge_list(graph_file);
+
+	int best_clique = 0;
+	double total_time = 0.0;
+	for (int i = 0; i < num_executions; ++i)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+
+		SimulatedAnnealing sa(100.0, 0.001, 0.9995, g, 1);
+		std::cout << "Running simulated annealing algorithm..." << std::endl;
+		std::vector<int> clique = sa.run();
+		int clique_size_sa = clique.size();
+		std::cout << "Clique size sa: " << clique_size_sa << std::endl;
+
+		int cur_clique = clique_size_sa;
+		if (cur_clique > best_clique)
+		{
+			best_clique = cur_clique;
+		}
+		auto end = std::chrono::high_resolution_clock::now();
+
+		total_time += std::chrono::duration<double>(end - start).count();
+		double exec_time = std::chrono::duration<double>(end - start).count();
+
+		out << (i + 1) << "," << graph_file_pretty << "," << cur_clique << "," << exec_time << std::endl;
+	}
+	std::cout << "Melhor clique em 20 execucoes = " << best_clique << std::endl;
+	std::cout << "Tempo de execucao total: " << total_time << " seconds" << std::endl;
+}
 
 void test(const std::string &graph_file, int num_executions, const std::string &output_file)
 {
@@ -91,7 +130,7 @@ void test_all_files_in_directory(const std::string &directory_path, int num_exec
 			{
 				std::string file_path = directory_path + "/" + file_name;
 				std::cout << "Testing file: " << file_path << std::endl;
-				test(file_path, num_executions, output_file);
+				control_test(file_path, num_executions, output_file);
 			}
 		}
 		closedir(dir);
